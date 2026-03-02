bda09bd372de1bd6e50086395d04174df311186b
Taha Hussain <65211376+tahasaifeee@users.noreply.github.com>
2026-03-02 15:29:53 +0400

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = "your-secret-key-please-change-in-production"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: Union[List[AnyHttpUrl], str] = []
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
             return [i.strip() for i in v.split(",")]
         elif isinstance(v, (list, str)):
             return v
-        raise ValueError(v)
+        return v
 
     POSTGRES_SERVER: str
     POSTGRES_USER: str
