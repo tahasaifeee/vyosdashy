1c7aa168ee96150e12f1e9adf4caba30a66b1d18
Taha Hussain <65211376+tahasaifeee@users.noreply.github.com>
2026-03-02 18:11:07 +0400

    error

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -15,8 +15,9 @@ class Settings(BaseSettings):
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
+        elif isinstance(v, str) and v.startswith("["):
+            import json
+            return json.loads(v)
         return v
 
     POSTGRES_SERVER: str
--- backend/app/main.py
+++ backend/app/main.py
@@ -36,9 +36,13 @@ app = FastAPI(
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
+    origins = settings.BACKEND_CORS_ORIGINS
+    if isinstance(origins, str):
+        origins = [origins]
+    
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=[str(origin).rstrip("/") for origin in origins],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
